2182e868a96dd89f0f211e4ee87367a9270bfaee
Katarzyna Bogumil <katarzyna.bogumil@gmail.com>
2023-12-17 13:44:28 +0100

    Fix row and column names

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ shortestPath(maze, [start.x,start.y], [end.x,end.y]); // could change input to n
 
 
 function shortestPath(maze, [a, b], [x, y]) {
-  let start = maze[a][b]; // if nodes as input - not necessary
-  let end = maze[x][y];
+  let start = maze[b][a]; // if nodes as input - not necessary
+  let end = maze[y][x];
   // if no generatePoint used - check if points are not walls!
   // if (start.wall || end.wall) return console.log('Either start or target are walls - no path possible.');
   
@@ -64,7 +64,7 @@ function shortestPath(maze, [a, b], [x, y]) {
 
 
 function setupMaze(sizeX, sizeY, noOfWalls) {
-  let maze = Array.from(Array(sizeX), (element, indexX) => Array.from(Array(sizeY), (element, indexY) => new Node(indexX, indexY)));
+  let maze = Array.from(Array(sizeY), (_, indexY) => Array.from(Array(sizeX), (__, indexX) => new Node(indexX, indexY)));
 
   maze.flat(1)
     .map(node => ({
@@ -74,7 +74,7 @@ function setupMaze(sizeX, sizeY, noOfWalls) {
     .sort((a, b) => a.sort - b.sort)
     .map(node => node.value)
     .slice(0, noOfWalls)
-    .forEach(node => maze[node.x][node.y].wall = true);
+    .forEach(node => maze[node.y][node.x].wall = true);
 
   maze.forEach(row => row.forEach((el) => el.neighbours.push(...getNeibours(el, maze))));
 
@@ -83,13 +83,13 @@ function setupMaze(sizeX, sizeY, noOfWalls) {
 
 
 function getNeibours(el, maze) {
-  let sizeX = maze.length;
-  let sizeY = maze[0].length;
+  let sizeX = maze[0].length;
+  let sizeY = maze.length;
   let neighbours = [];
   for (let row = Math.max(el.x - 1, 0); row < Math.min(el.x + 2, sizeX); row++) {
     for (let col = Math.max(el.y - 1, 0); col < Math.min(el.y + 2, sizeY); col++) {
-      if (!(maze[row][col].wall) && maze[row][col] !== el && (row === el.x || col === el.y)) { // no diags
-        neighbours.push(maze[row][col]);
+      if (!(maze[col][row].wall) && maze[col][row] !== el && (row === el.x || col === el.y)) { // no diags
+        neighbours.push(maze[col][row]);
       }
     }
   }
@@ -98,26 +98,21 @@ function getNeibours(el, maze) {
 
 
 function generatePoint(maze) {
-  let sizeX = maze.length;
-  let sizeY = maze[0].length;
+  let sizeX = maze[0].length;
+  let sizeY = maze.length;
   let point;
   do {
-    point = maze[~~(Math.random()*sizeX)][~~(Math.random()*sizeY)]
+    point = maze[~~(Math.random()*sizeY)][~~(Math.random()*sizeX)]
   } while (point.wall !== false)
   return point;
 }
 
 
 function print(maze) {
-  // console.table(maze.map(row => row.map(node => {
-  //   if (node.wall) return 'X';
-  //   else return ' ';
-  // })));
-
-  for (let i = 0; i < maze.length; i++) {
+  for (let j = 0; j < maze.length; j++) {
     let row = '';
-    for (let j = 0; j < maze[0].length; j++) {
-      let node = maze[i][j].wall ? String.fromCodePoint(11035) : String.fromCodePoint(11036); // 11093 - red circle, 11088 - star
+    for (let i = 0; i < maze[0].length; i++) {
+      let node = maze[j][i].wall ? String.fromCodePoint(11035) : String.fromCodePoint(11036); // 11093 - red circle, 11088 - star
       row += '' + node;
     }
     console.log(row);
@@ -125,10 +120,10 @@ function print(maze) {
 }
 
 function printPath(maze, path) {
-  for (let i = 0; i < maze.length; i++) {
-    let row = ''; 
-    for (let j = 0; j < maze[0].length; j++) {
-      let el = maze[i][j];
+  for (let j = 0; j < maze.length; j++) {
+    let row = '';
+    for (let i = 0; i < maze[0].length; i++) {
+      let el = maze[j][i];
       let node = el.wall ? String.fromCodePoint(11035) : String.fromCodePoint(11036);
       if (path.includes(el)) node = String.fromCodePoint(11093);
       if (el === path[0]) node = String.fromCodePoint(11088);
